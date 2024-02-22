afb3d120ff12a227e81ce845e81b30eb6e99da65
elzaw <hoodaelzaw81@gamil.com>
2024-02-22 14:44:27 +0200

    edits

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -49,29 +49,27 @@ const Home = () => {
         {/* Products */}
         <Carousel
           responsive={responsive}
-          className="gap-4  p-2 mb-2 rounded bg-white mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8 items-center"
+          className="gap-4 p-2 mb-2 rounded bg-white mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8 items-center"
         >
           {products.map((product) => (
             <div key={product.id} to={product.id}>
-
-              <div className="hover:scale-[1.01] group relative mx-2" >
-
-              <div className="hover:scale-[1.01] group relative">
-
-                <div className="w-52 h-52 overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
-                  <img
-                    src={product.images}
-                    alt={product.name}
-                    onClick={() => {
-                      navigate(`/product/${product.product_id}`);
-                    }}
-                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4 justify-between">
-                  <h3 className="text-lg  leading-6 font-medium text-gray-900">
-                    {product.name}
-                  </h3>
+              <div className="hover:scale-[1.01] group relative mx-2">
+                <div className="hover:scale-[1.01] group relative">
+                  <div className="w-52 h-52 overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
+                    <img
+                      src={product.images}
+                      alt={product.name}
+                      onClick={() => {
+                        navigate(`/product/${product.product_id}`);
+                      }}
+                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                    />
+                  </div>
+                  <div className="mt-4 justify-between">
+                    <h3 className="text-lg  leading-6 font-medium text-gray-900">
+                      {product.name}
+                    </h3>
+                  </div>
                 </div>
               </div>
             </div>
