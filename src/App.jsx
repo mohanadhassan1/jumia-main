cee53a8197ab5cde4cd8cd8417b06dbf31ec105a
elzaw <hoodaelzaw81@gamil.com>
2024-02-26 23:02:01 +0200

    check

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,8 @@ import store from "./store/store";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 import AppLayout from "./AppLayout";
-<<<<<<< HEAD
 // import ProductDetails from "./components/product/productDetails";
-=======
 
->>>>>>> 69f02242518796f2cb7a6bb427f3d282a02dd1f3
 import Home from "./components/Home/index";
 import ProductDetails from "./components/product/productDetails";
 
@@ -33,10 +30,7 @@ const routes = createBrowserRouter([
       { path: "/cart", element: <Cart /> },
       { path: "/myaccount", element: <MyAccount /> },
       { path: "/product/:id", element: <ProductDetails /> },
-<<<<<<< HEAD
-=======
 
->>>>>>> 69f02242518796f2cb7a6bb427f3d282a02dd1f3
       { path: "/help", element: <HelpMain /> },
 
     ],
