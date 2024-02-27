bc98463ed658391ac0b9c07d729d769cf583ff35
yousabemad <usabemad40@gmail.com>
2024-02-27 23:49:10 +0200

    add Selector dropdown

--- src/components/SupermarketCat/SupermarkerCategory.jsx
+++ src/components/SupermarketCat/SupermarkerCategory.jsx
@@ -5,8 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../../store/slices/products";
 import SmallCart from "../Small Cart/SmallCart";
-import {Dropdown} from "flowbite"; 
-
 
 export default function SupermarkerCategory() {
   const { products } = useSelector((state) => state.products);
@@ -42,16 +40,24 @@ export default function SupermarkerCategory() {
 
   const [name, setName] = useState("Supermarket");
 
-
-
   function changeCategories(newName, event) {
     event.preventDefault();
     setName(newName);
   }
 
-   
+  const [selectSort, setselectSort] = useState("Popularity");
+  const [isOpen, setIsOpen] = useState(false);
 
-  
+  const toggleDropdown = () => {
+    event.preventDefault();
+
+    setIsOpen(!isOpen);
+  };
+  function changeSelect(newSelect, event) {
+    event.preventDefault();
+    setselectSort(newSelect);
+    setIsOpen(!isOpen);
+  }
   return (
     <>
       <div className="md:container h-full mx-auto items-center justify-center">
@@ -325,9 +331,103 @@ export default function SupermarkerCategory() {
           </div>
 
           <div className="md:flex flex-wrap  gap-4 p-4 mb-3 rounded bg-white">
-            <div className=" w-full">
+            <div className="flex justify-between items-center w-full">
               <h2>{name}</h2>
-       </div>
+              <div className="relative">
+                <button
+                  id="dropdownDefaultButton"
+                  className="  hover: focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  type="button"
+                  onClick={toggleDropdown}
+                >
+                  <label htmlFor="">Sort by:</label>
+                  {selectSort}{" "}
+                  <svg
+                    className={`w-2.5 h-2.5 ms-3 ${
+                      isOpen ? "transform rotate-180" : ""
+                    }`}
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                {isOpen && (
+                  <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul
+                      className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      aria-labelledby="dropdownDefaultButton"
+                    >
+                      <li
+                        
+                      >
+                        <a
+                          href=""
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          onClick={() =>
+                            changeSelect("Popularity", event)
+                          }
+                       >
+                          Popularity
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() =>
+                            changeSelect("Newest Arrivals", event)
+                          }
+                        >
+                          Newest Arrivals
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() =>
+                            changeSelect(" Price: Low to High", event)
+                          }
+                        >
+                          Price: Low to High
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() =>
+                            changeSelect("Price: High to Low", event)
+                          }
+                        >
+                          Price: High to Low
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href=""
+                          className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                        onClick={() =>
+                            changeSelect("  Product Rating", event)
+                          }
+                        >
+                          Product Rating
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
             {products.map((product) => (
               <div
                 className="md:w-1/2 lg:w-[30%] xl:w-[15%] mb-4 "
@@ -337,14 +437,10 @@ export default function SupermarkerCategory() {
               >
                 <SmallCart Image={product.images} title={product.name} />
               </div>
-
             ))}
-                        </div>
-
           </div>
         </div>
-     
-       
+      </div>
     </>
   );
 }
