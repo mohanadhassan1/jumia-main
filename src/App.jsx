e5454070859793f67ccb36a363f6a7b7686a28ef
mohanad <mohanadhassan246@gmail.com>
2024-02-27 10:02:50 +0200

    editable

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import Cart from "./components/Cart/Cart";
 import AppLayout from "./AppLayout";
 
 import Home from "./components/Home/index";
-import ProductDetails from "./components/product/productDetails";
-
+import ProductDetails from "./components/product/ProductDetails";
+import Trash from "./components/product/Trash";
 import MyAccount from "./components/MyAccount";
 import Login from "./components/Login/index";
 import HelpMain from "./components/Help";
 import SupermarkerCategory from "./components/SupermarketCat/SupermarkerCategory";
+import { BiTrash } from "react-icons/bi";
 
 const routes = createBrowserRouter([
   {
@@ -24,6 +25,7 @@ const routes = createBrowserRouter([
       { path: "/myaccount", element: <MyAccount /> },
       
       { path: "/product/:id", element: <ProductDetails /> },
+      { path: "/trash", element: <Trash /> },
 
       { path: "/help", element: <HelpMain /> },
       { path: "/Supremarket", element: <SupermarkerCategory /> },
--- src/components/product/productDetails.jsx
+++ src/components/product/productDetails.jsx
@@ -9,8 +9,8 @@ export default function ProductDetails() {
     const [product, setProduct] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const dispatch = useDispatch(); 
-    const navigate=useNavigate()
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function getData() {
@@ -43,21 +43,32 @@ export default function ProductDetails() {
     }
 
     return (
-        <div style={{ display: "flex", justifyContent: "space-evenly", margin: "20px" }}>
-            <div>
-                <img style={{ width: "400px", height: "400px" }} src={product.images} alt={product.name} />
-            </div>
-            <div style={{ width: "300px", height: "400px" }} className='bg-white  p-3'>
-                <h2>{product.name}</h2>
-                <p className="text-sm text-gray-900 ">{product.description}</p>
-             <p className="text-sm text-gray-900 "> Quantity: {product.quantity_in_stock}</p>
+        <>
+            <div className='flex container mx-auto gap-4 mt-3 h-96'>
+                <div className='bg-white w-3/4'>
 
-                <p> EGY {product.price}</p>
-                <p>{product.quantity}</p>
-                <button onClick={handleAddToCart}
-                 className="button bg-orange-600 w-4/4 hover:bg-orange-700 text-white m-4 font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
-                >Add to Cart</button> {/* Button to add product to cart */}
+                </div>
+                <div className='bg-white w-1/4'>
+
+                </div>
             </div>
-        </div>
+
+            <div style={{ display: "flex", justifyContent: "space-evenly", margin: "20px" }}>
+                <div>
+                    <img style={{ width: "400px", height: "400px" }} src={product.images} alt={product.name} />
+                </div>
+                <div style={{ width: "300px", height: "400px" }} className='bg-white  p-3'>
+                    <h2>{product.name}</h2>
+                    <p className="text-sm text-gray-900 ">{product.description}</p>
+                    <p className="text-sm text-gray-900 "> Quantity: {product.quantity_in_stock}</p>
+
+                    <p> EGY {product.price}</p>
+                    <p>{product.quantity}</p>
+                    <button onClick={handleAddToCart}
+                        className="button bg-orange-600 w-4/4 hover:bg-orange-700 text-white m-4 font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline"
+                    >Add to Cart</button> {/* Button to add product to cart */}
+                </div>
+            </div>
+        </>
     );
 }
