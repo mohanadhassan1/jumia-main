fd504829ae05c8a4a5b0b92736fd82d719c46484
Mahmoud Elzaw <hoodaelzaw81@gmail.com>
2024-04-05 02:32:39 +0200

    fix add to cart

--- src/components/product/productDetails.jsx
+++ src/components/product/productDetails.jsx
@@ -9,7 +9,6 @@ import { FaFacebookF } from "react-icons/fa";
 import { TiSocialTwitter } from "react-icons/ti";
 import { TiSocialLinkedin } from "react-icons/ti";
 
-
 export default function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState({});
@@ -101,11 +100,15 @@ export default function ProductDetails() {
                 <hr />
                 <h2 className="">SHARE THIS PRODUCT</h2>
                 <div className="flex">
-                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800"><FaFacebookF /></button>
-                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800"><TiSocialTwitter /></button>
-                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800"><TiSocialLinkedin /></button>
-
-
+                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800">
+                    <FaFacebookF />
+                  </button>
+                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800">
+                    <TiSocialTwitter />
+                  </button>
+                  <button className="p-1 m-1 border border-blue-900 rounded-full hover:text-orange-800 hover:border-orange-800">
+                    <TiSocialLinkedin />
+                  </button>
                 </div>
               </div>
 
@@ -124,8 +127,9 @@ export default function ProductDetails() {
                       <button
                         key={size}
                         onClick={() => handleSizeSelect(size)}
-                        className={`mr-2 px-4 py-2 border border-gray-300 rounded ${selectedSize === size ? "bg-gray-200" : "bg-white"
-                          }`}
+                        className={`mr-2 px-4 py-2 border border-gray-300 rounded ${
+                          selectedSize === size ? "bg-gray-200" : "bg-white"
+                        }`}
                       >
                         {size}
                       </button>
@@ -146,7 +150,6 @@ export default function ProductDetails() {
           <div className="bg-white md:w-1/4 md:mt-0 sm:mt-3 p-3">
             <h4>DELIVERY & RETURNS</h4>
             <hr />
-
           </div>
         </div>
 
