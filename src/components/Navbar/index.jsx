2b641e6672867eb3bdf38796d7e9f8885f12ce1c
mohanad <mohanadhassan246@gmail.com>
2024-04-16 05:41:24 +0200

    myaccount edit

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -242,7 +242,7 @@ const Navbar = () => {
                         </div>
                       </a>
                       <a
-                        href="#"
+                        href="/myaccount"
                         className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-300 hover:font-bold"
                         role="menuitem"
                         tabIndex="-1"
--- src/store/slices/customer.js
+++ src/store/slices/customer.js
@@ -2,7 +2,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import instance from './../../axois/instance';
 
-export const userAction = createAsyncThunk("customer/getCustomer", async (id) => {
+export const customerAction = createAsyncThunk("customer/getCustomer", async (id) => {
   const res = await instance.get(`/customers/${id}`);
   return res.data;
 });
@@ -12,19 +12,14 @@ const customerSlice = createSlice({
     name: "customer",
     initialState: { customer: {} },
     extraReducers: (builder) => {
-        builder.addCase(userAction.fulfilled, (state, action) => {
+        builder.addCase(customerAction.fulfilled, (state, action) => {
             console.log(action.payload);
             state.customer = action.payload
         })
-        builder.addCase(userAction.rejected, (state, action) => {
+        builder.addCase(customerAction.rejected, (state, action) => {
             console.log("rejected");
         })
-        // builder.addCase(userEditAction.fulfilled, (state, action) => {
-        //     console.log(action.payload);
-        // })
-        // builder.addCase(userEditAction.rejected, (state, action) => {
-        //     console.log("rejected");
-        // })
+
     }
 })
 
