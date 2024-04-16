6668faf984981e90dec59d998e182c445d3e2a36
yousabemad <usabemad40@gmail.com>
2024-04-16 19:12:46 +0200

    edit the SmallCart

--- src/components/Small Cart/SmallCart.jsx
+++ src/components/Small Cart/SmallCart.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 export default function SmallCart({ Image, title, price }) {
   const maxLength =20; // Define the maximum length for the title
 
@@ -8,14 +9,11 @@ export default function SmallCart({ Image, title, price }) {
     return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
   };
 
+
+
   return (
     <>
-      {/* <div className="hover:scale-[1.01] h-[15rem] w-full m-1 p-2 pb-2 rounded-xl overflow-hidden shadow-lg flex flex-col">
-        <img className="w-full p-1" src={Image} alt="" />
-        <h3>{truncateTitle(title, maxLength)}</h3>
-        
-        <IoIosAddCircle className="text-[#F57C00] text-3xl mt-1 cursor-pointer absolute top-0 end-0" />
-      </div> */}
+     
 
       <div class="max-w-2xl mx-auto  ">
         <div class="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
@@ -77,13 +75,8 @@ export default function SmallCart({ Image, title, price }) {
               <span class="text-3xl font-bold text-gray-900 dark:text-white">
                 $ {price}
               </span>
-              <a
-                href="#"
-                class="text-white bg-orange-700  focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Add to cart
-              </a>
             </div>
+              
           </div>
         </div>
       </div>
