6c89dafc6e7e50993b906733e26532549824ff5f
yousabemad <usabemad40@gmail.com>
2024-04-18 16:22:05 +0200

    small cart

--- src/store/store.js
+++ src/store/store.js
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/products";
 import cartReducer from "./slices/cart"
 import authSliceReducer from "./slices/authSlice";
-import idCategoryReducer from "./slices/idCategory"
+import idCategoryReducer from "./slices/idCategory";
+import idCategoryPageReducer from "./slices/productsCategory";
 
 const store = configureStore({
   reducer: {
     products: productReducer,
     cart:cartReducer,
     auth:authSliceReducer,
-    idCategory:idCategoryReducer
+    idCategory:idCategoryReducer,
+    paginationProduct:idCategoryPageReducer,
   },
 });
 
